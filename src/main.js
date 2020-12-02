85eab2b2886615c31e7f25709c75e2cd7015afc2
JacobBarnard <jacob.barnard21@gmail.com>
2020-12-02 11:41:14 -0700

    deployment prep

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './routes.js'
 import axios from 'axios'
+import store from './store.js'
 
-// axios.defaults.baseURL = "https://cis410-fa20-rockwellapi.azurewebsites.net"
+axios.defaults.baseURL = "https://cis410-fa20-barnardlofquist-api-3.azurewebsites.net"
 
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
